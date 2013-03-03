fbde6a39281dfd625f0a8c00298501236e3bbe50
David Liman <limanoit@gmail.com>
2013-03-03 02:36:59 -0500

    try to serialize struct to json

--- kchat/kchat.go
+++ kchat/kchat.go
@@ -2,32 +2,54 @@ package kchat
  
 import (
   "appengine"
-  "appengine/channel"
+  "appengine/datastore"
 
+  "encoding/json"
   "html/template"
-  // "fmt"
   "net/http"
+  "time"
+  "fmt"
 )
+
+type Message struct {
+  Name  string
+  Body  string 
+  Date  time.Time
+}
 
 func init() {
   http.HandleFunc("/", root)
+  http.HandleFunc("/chat", chat)
 }
 
-var mainTemplate = template.Must(template.ParseFile("main.html"))
-
 func root(w http.ResponseWriter, r *http.Request) {
-  context := appengine.NewContext(r)
-  token, err := channel.Create(context, "kchat")
-  if err != nil {
-    http.Error(w, "Could not create channel", http.StatusInternalServerError)
-    context.Errorf("channel.Create: %v", err)
+  var mainTemplate = template.Must(template.ParseFiles("main.html"))
+
+  if err := mainTemplate.Execute(w, map[string] string {}); err != nil {
+    http.Error(w, err.Error(), http.StatusInternalServerError)
     return
   }
+}
 
-  err = mainTemplate.Execute(w, map[string] string { "token": token })
-  if err != nil {
-    context.Errorf("mainTemplate: %v", err)
+func chat(w http.ResponseWriter, r *http.Request) {
+  context := appengine.NewContext(r)
+
+  if r.Method == "GET" {
+    q := datastore.NewQuery("Message").Order("-Date").Limit(50)
+    messages := make([] Message, 0, 50)
+
+    if _, err := q.GetAll(context, &messages); err != nil {
+      http.Error(w, err.Error(), http.StatusInternalServerError)
+      return
+    }
+    
+    w.Header().Set("Content-Type", "application/json")
+    fmt.Fprint(w, json.Marshal(messages))
+    return
   }
-  
+ 
+  if r.Method == "POST" {
+
+  }
 }
 
